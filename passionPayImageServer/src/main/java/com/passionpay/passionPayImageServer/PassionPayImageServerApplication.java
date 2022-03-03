33f7835e3d6bc8a5da950159c50f6499e09e0cc3
Seungku Kim <seungkukim@kaist.ac.kr>
2022-03-03 23:36:04 +0900

    1st commit

--- passionPayImageServer/src/main/java/com/passionpay/passionPayImageServer/PassionPayImageServerApplication.java
+++ passionPayImageServer/src/main/java/com/passionpay/passionPayImageServer/PassionPayImageServerApplication.java
@@ -7,15 +7,8 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 @SpringBootApplication
 public class PassionPayImageServerApplication {
 
-	public static final String APPLICATION_LOCATIONS = "spring.config.location="
-			+ "classpath:application.yml,"
-			+ "classpath:aws.yml";
-
 	public static void main(String[] args) {
-//		SpringApplication.run(PassionPayImageServerApplication.class, args);
-		new SpringApplicationBuilder(PassionPayImageServerApplication.class)
-				.properties(APPLICATION_LOCATIONS)
-				.run(args);
+		SpringApplication.run(PassionPayImageServerApplication.class, args);
 	}
 
 }
